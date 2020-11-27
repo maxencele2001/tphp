f5887131596b791fe25d22c14a7246b444c27c9c
Maxence <maxence.crosse@ynov.com>
2020-11-27 17:33:20 +0100

    Panier fait

---
+++ src/Controller/CartController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use App\Service\Cart\CartService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CartController extends AbstractController
+{
+    /**
+     * @Route("/cart", name="cart.index")
+     */
+    public function index(CartService $cartService): Response
+    {
+        return $this->render('cart/index.html.twig',[
+            'items'=> $cartService->getFullCart(),
+            'total'=> $cartService->getTotal(),
+        ]);
+    }
+
+    /**
+     * @Route("/cart/add/{id}", name="cart.add")
+     */
+    public function add($id, CartService $cartService)
+    {
+        $cartService->add($id);
+        return $this->redirectToRoute('cart.index');
+    }
+
+    /**
+     * @Route("/cart/remove/{id}", name="cart.remove")
+     */
+    public function remove($id,CartService $cartService)
+    {
+        $cartService->remove($id);
+        return $this->redirectToRoute('cart.index');
+    }
+}
---
+++ src/Service/Cart/CartService.php
@@ -0,0 +1,64 @@
+<?php 
+
+namespace App\Service\Cart;
+
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class CartService{
+
+    protected $session;
+
+    public function __construct(SessionInterface $session, ProductRepository $repo)
+    {
+        $this->session = $session;
+        $this->repo = $repo;
+    }
+
+    public function add(int $id)
+    {
+        $panier = $this->session->get('panier', []);
+
+        if(!empty($panier[$id])){
+            $panier[$id]++;
+        }else{
+            $panier[$id] = 1;
+        }
+        $this->session->set('panier',$panier);
+
+    }
+
+    public function remove(int $id)
+    {
+        $panier = $this->session->get('panier', []);
+
+        if(!empty($panier[$id])){
+            unset($panier[$id]);
+        }
+        $this->session->set('panier',$panier);
+    }
+
+    public function getFullCart():array 
+    {
+        $panier = $this->session->get('panier', []);
+        $panierWithData = [];
+
+        foreach($panier as $id=>$quantity){
+            $panierWithData[] =[ 
+                'product' => $this->repo->find($id),
+                'quantity'=> $quantity
+            ];
+        }
+
+        return $panierWithData;
+    }
+
+    public function getTotal():float 
+    {
+        $total = 0;
+        foreach($this->getFullCart() as $item){
+            $total += $item['product']->getPriceHT() * $item['quantity'];
+        }
+        return $total;
+    }
+}
